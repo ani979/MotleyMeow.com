b76d64ea94a6fdeb450f051070c258f2c2b53b03
ani979 <ani979@gmail.com>
2015-03-17 21:48:06 +0530

    check email from FB

--- server_buzzingartist/app.js
+++ server_buzzingartist/app.js
@@ -136,7 +136,11 @@ passport.use(new FacebookStrategy({
                         newUser.facebook.token = profile.token; // we will save the token that facebook provides to the user                    
                         newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
                         console.log("email id " + newUser.facebook.name);
-                        newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                        if(typeof profile.emails == 'undefined' || profile.emails.length == 0) {
+                            newUser.facebook.email = "";
+                        } else {
+                            newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first    
+                        }
                         newUser.local.picture  = "https://graph.facebook.com/" + profile.id + "/picture" + "?width=200&height=200" + "&access_token=" + accessToken + '&appsecret_proof=' + req.session.hashValue;
                         newUser.local.joiningDate    = new Date(); 
                         FB.api(
