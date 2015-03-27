ef90b23dd1454e0c4a8a957320b00f883ce1d369
ani979 <ani979@gmail.com>
2015-03-27 22:20:22 +0530

    Added a few more roles

--- server_buzzingartist/views/js/theatreContrib.js
+++ server_buzzingartist/views/js/theatreContrib.js
@@ -2,7 +2,8 @@
 module.exports = {alldropdowns: {
 					roles: [{ role: "Actor", text:"Acting"}, { role: "Director", text:"Direction"}, { role: "Playwright", text:"Playwright"},
                             { role: "Lights", text:"Lighting Design"},{ role: "Sound", text:"Sound Design"},
-    						{ role: "Music", text:"Music Composition"},{ role: "Costume", text:"Costume Designing"},
+    						{ role: "Music", text:"Music Composition"},{ role: "Dramaturgist", text:"Dramaturgy"},{ role: "Costume", text:"Costume Designing"},
+                            { role: "MakeUp", text:"Make-up"},
     						{ role: "SetDesign", text:"Set Designing"},{ role: "StageManager", text:"Managing Production"}, 
                             { role: "Backstage", text:"Production/Backstage Crew"}, { role: "Trainer", text:"Training"}],
     				cities: [{ city: "Bengaluru", text: "Bengaluru"}, {city:"Chennai", text:"Chennai"}, 
@@ -10,8 +11,8 @@ module.exports = {alldropdowns: {
     				citiesForPost: [{ city: "Bengaluru", text: "Bengaluru"}, {city:"Chennai", text:"Chennai"}, 
     						 {city: "Kolkata", text:"Kolkata"},{city:"Delhi", text:"Delhi"}, {city: "Mumbai", text: "Mumbai"}],
     				rolesForPost: [{ role: "Actor", text:"Actor"}, { role: "Director", text:"Director"}, { role: "Playwright", text:"Playwright"},
-                            { role: "Lights", text:"Lighting Designer"},{ role: "Sound", text:"Sound Designer"},
-                            { role: "Music", text:"Music Composer"},{ role: "Costume", text:"Costume Designer"},
+                            { role: "Lights", text:"Lighting Designer"},{ role: "Sound", text:"Sound Designer"},  { role: "MakeUp", text:"Make-up artist"},
+                            { role: "Music", text:"Music Composer"},{ role: "Dramaturgist", text:"Dramaturgist"}, { role: "Costume", text:"Costume Designer"},
                             { role: "SetDesign", text:"Set Designer"},{ role: "StageManager", text:"Production Manager"}, 
                             { role: "Backstage", text:"Backstage Support"}, { role: "Trainer", text:"Trainer"} ,{ role: "Props", text:"Props"},
     						{ role: "RehearsalSpace", text:"Rehearsal Space"}],
@@ -20,8 +21,8 @@ module.exports = {alldropdowns: {
     							   { category: "Storytelling", text:"Storytelling event"}, {category: "Reading", text:"Reading"},
     							   {category: "Others", text:"Other Events"}],
                     rolesForSearch: [{ role: "Actor", text:"Actor"}, { role: "Director", text:"Director"}, { role: "Playwright", text:"Playwright"},
-                            { role: "Lights", text:"Lighting Designer"},{ role: "Sound", text:"Sound Designer"},
-                            { role: "Music", text:"Music Composer"},{ role: "Costume", text:"Costume Designer"},
+                            { role: "Lights", text:"Lighting Designer"}, { role: "Dramaturgist", text:"Dranamturgist"},{ role: "Sound", text:"Sound Designer"},
+                            { role: "Music", text:"Music Composer"},{ role: "Costume", text:"Costume Designer"}, { role: "MakeUp", text:"Make-up artist"},
                             { role: "SetDesign", text:"Set Designer"},{ role: "StageManager", text:"Production Manager"}, 
                             { role: "Backstage", text:"Backstage Support"}, { role: "Trainer", text:"Trainer"} ,{ role: "Audience", text:"Audience"}]
     			}
