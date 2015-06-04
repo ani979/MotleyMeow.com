81a1ccd1b4f9652e001a7006cdf18a203defcfee
Shubham Arora <shubhamarora2k13@live.com>
2015-06-04 21:12:03 +0530

    no message

--- server_buzzingartist/app.js
+++ server_buzzingartist/app.js
@@ -344,53 +344,6 @@ passport.use(new GoogleStrategy({
 
     }));
 
- passport.use(new TwitterStrategy({
-
-        consumerKey     : config.twitterAuth.consumerKey,
-        consumerSecret  : config.twitterAuth.consumerSecret,
-        callbackURL     : config.twitterAuth.callbackURL
-
-    },
-    function(token, tokenSecret, profile, done) {
-
-        // make the code asynchronous
-    // User.findOne won't fire until we have all our data back from Twitter
-        process.nextTick(function() {
-
-            User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-
-                // if there is an error, stop everything and return that
-                // ie an error connecting to the database
-                if (err)
-                    return done(err);
-
-                // if the user is found then log them in
-                if (user) {
-                    return done(null, user); // user found, return that user
-                } else {
-                    // if there is no user, create them
-                    var newUser                 = new User();
-
-                    // set all of the user data that we need
-                    newUser.facebook.id          = profile.id;
-                    newUser.facebook.token       = token;
-                    newUser.facebook.name    =     profile.displayName;
-                    newUser.facebook.email =       profile.email;
-
-                    newUser.local.joiningDate  = Date();
-                    newUser.local.password='0';
-
-                    // save our user into the database
-                    newUser.save(function(err) {
-                        if (err)
-                            throw err;
-                        return done(null, newUser);
-                    });
-                }
-            });
-
-    });
-}));
 
 var app = express();
 
