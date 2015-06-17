f5ab2520d6d3109f5312237e82391c891926ea38
Shubham Arora <shubhamarora2k13@live.com>
2015-06-17 11:28:30 +0530

    no message

--- server_buzzingartist/app.js
+++ server_buzzingartist/app.js
@@ -479,9 +479,9 @@ app.get('/AddSpace', function(req, res) {
              console.log(req.body.nameofspace);
         
                 
-              Space.findOne({ 'space.spaceId' : req.body.nameofspace }, function(error, db) {
+              Space.findOne({ 'space.spaceName' : req.body.nameofspace }, function(error, db) {
             
-                res.render('performanceView', {  space: db, dropdowns:dropdowns, sessionSpace: req.session.space });
+                res.render('performanceView', {  space: db, dropdowns:dropdowns });
             });
               });    
   
